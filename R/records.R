d7aef662fd66c82741e80b5ca09a5f3cf4c3cb33
pieterprovoost <pieterprovoost@gmail.com>
2015-09-12 01:10:38 +0200

    added paging support

--- R/records.R
+++ R/records.R
@@ -1,21 +1,28 @@
 records <- function(scientificname, like=TRUE, fuzzy=TRUE, marine_only=FALSE, verbose=FALSE) {
 
-  values <- list(scientificname=scientificname, fuzzy=fuzzy, marine_only=marine_only)
-  response <- .request("getAphiaRecords", values)
-
-  if (verbose) {
-    print(response)
-  }
-
-  nodeset <- xpathApply(response, "//item")
-
   result <- NULL
+  offset <- 1
 
-  for (node in nodeset) {
-    l <- .list(node)
-    result <- rbind(result, as.data.frame(l))
+  while (TRUE) {
+
+    values <- list(scientificname=scientificname, fuzzy=fuzzy, marine_only=marine_only, offset=offset)
+    response <- .request("getAphiaRecords", values)
+    if (verbose) {
+      print(response)
+    }
+    nodeset <- xpathApply(response, "//item")
+
+    for (node in nodeset) {
+      l <- .list(node)
+      result <- rbind(result, as.data.frame(l))
+    }
+
+    if (length(nodeset) < 50) {
+      return(result)
+    } else {
+      offset <- offset + 50
+    }
+
   }
-
-  return(result)
 
 }
